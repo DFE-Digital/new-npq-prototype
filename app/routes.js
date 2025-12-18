48c828f47870f46927cc1e6f346637cb3c0143fa
Emma Frith <emma.frith@education.gov.uk>
2025-12-18 16:30:17 +0000

    Add another funded route

--- app/routes.js
+++ app/routes.js
@@ -6,21 +6,26 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
-
-// Run this code when a form is submitted to 'select-npq'
 router.post('/select-npq', function (req, res) {
 
-    // Get the value from the radio buttons
     var startMonth = req.session.data['course-start']
 
-    // Check which option the user selected
     if (startMonth === "April 2026") {
-        // Send user to the November-specific page
         res.redirect('/select-npq')
     } else {
-        // Send user to the default April page
         res.redirect('/check-funding-start')
+    }
+
+})
+
+router.post('/select-npq-funding-check', function (req, res) {
+
+    var workEngland = req.session.data['england']
+
+    if (workEngland === "Yes") {
+        res.redirect('/select-npq-funding-check')
+    } else {
+        res.redirect('/not-eligible-for-funding-england')
     }
 
 })
