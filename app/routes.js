c4f165af72f641e50e4d353bf0aeda52e6430e66
Emma Frith <emma.frith@education.gov.uk>
2025-12-19 16:22:05 +0000

    Start adding funding logic

--- app/routes.js
+++ app/routes.js
@@ -29,3 +29,31 @@ router.post('/england-funding-check-answer', function (req, res) {
     }
 
 })
+
+
+router.post('/setting-funding-check-answer', function (req, res) {
+
+  var setting = req.session.data['setting-funding-check']
+
+  const schoolsDropdown = [
+    'School',
+    'Academy trust',
+    '16 to 19 setting',
+    'Nursery or preschool class',
+  ]
+
+   const ofsted = [
+    'Private nursery',
+    'Childminding setting',
+    'Other early years',
+  ]
+
+  if (schoolsDropdown.includes(setting)) {
+    res.redirect('/workplace')
+  } else if (ofsted.includes(setting)) {
+    res.redirect('/ofsted-number')
+  } else {
+    res.redirect('/not-eligible-for-funding-england')
+  }
+
+})
